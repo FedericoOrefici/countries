3b421cf413538561657d52a6bd4b4c9dcff732e7
FedericoOrefici <164881513+FedericoOrefici@users.noreply.github.com>
2024-09-16 16:41:10 -0300

    update

---
+++ README.md
@@ -0,0 +1,11 @@
+###############################################################################
+#Countries By Federico_Orefici
+
+1- To run de application, write npm i or npm install
+2- The run the backend type in the console:
+    node app
+
+3- To run the frontend type in the console:
+    npm run dev
+
+###############################################################################
--- server/src/services/countriesServices.js
+++ server/src/services/countriesServices.js
@@ -1,11 +1,6 @@
 const axios = require('axios');
 require('dotenv').config();
 
-// const API_URL = "https://date.nager.at/api/v3/AvailableCountries";
-// const API_URL_BY_ID = "https://date.nager.at/api/v3/CountryInfo";
-// const API_FLAGS = "https://countriesnow.space/api/v0.1/countries/flag/images";
-// const API_POPULATION = "https://countriesnow.space/api/v0.1/countries/population";
-
 exports.getAllCountries = async () => {
     try {
         const response = await axios.get(process.env.VITE_API_URL);
